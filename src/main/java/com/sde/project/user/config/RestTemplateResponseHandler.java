56d6d7dc331bcb8d49a3f63c6e807705e526d77c
ManuelaCorte <manu.cortepause@gmail.com>
2023-08-14 12:56:13 +0200

    Gateway endpoints for session creation and joining

---
+++ src/main/java/com/sde/project/user/config/RestTemplateResponseHandler.java
@@ -0,0 +1,42 @@
+package com.sde.project.user.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sde.project.user.models.responses.ExceptionResponse;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.DataRetrievalFailureException;
+import org.springframework.dao.PermissionDeniedDataAccessException;
+import org.springframework.http.client.ClientHttpResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.ResponseErrorHandler;
+
+import java.io.IOException;
+
+@Component
+public class RestTemplateResponseHandler implements ResponseErrorHandler {
+
+    @Override
+    public boolean hasError(ClientHttpResponse response) throws IOException {
+        return response.getStatusCode().is4xxClientError() || response.getStatusCode().is5xxServerError();
+    }
+
+    // throw new exception to be handled by the controller advice
+    @Override
+    public void handleError(ClientHttpResponse response) throws IOException {
+        ExceptionResponse exceptionResponse = new ObjectMapper().readValue(response.getBody(), ExceptionResponse.class);
+        switch (exceptionResponse.status()) {
+            case 400:
+                throw new IllegalArgumentException(exceptionResponse.message());
+            case 401:
+                throw new PermissionDeniedDataAccessException(exceptionResponse.message(), new Throwable(exceptionResponse.message()));
+            case 404:
+                throw new DataRetrievalFailureException(exceptionResponse.message());
+            case 409:
+                throw new DataIntegrityViolationException(exceptionResponse.message());
+            case 500:
+                throw new RuntimeException(exceptionResponse.message());
+            default:
+                throw new RuntimeException(exceptionResponse.message());
+        }
+    }
+}
+
--- src/main/java/com/sde/project/user/models/responses/SessionDetailsResponse.java
+++ src/main/java/com/sde/project/user/models/responses/SessionDetailsResponse.java
@@ -3,5 +3,5 @@ package com.sde.project.user.models.responses;
 import java.util.List;
 import java.util.UUID;
 
-public record SessionDetailsResponse(UUID id, String subject, String topic, String startTime, String endTime, RoomDetailsResponse room, List<FileResponse> files) {
+public record SessionDetailsResponse(UUID id, String subject, String topic, String startTime, String endTime, Boolean createdByUser, RoomDetailsResponse room, List<FileResponse> files) {
 }
